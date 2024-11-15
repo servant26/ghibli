5bccceef20c0ee527a25ed6d6ce8750b548560aa
servant26 <alirpl16@gmail.com>
2024-11-15 19:10:02 +0800

    update besar-besaran

--- detail/assets_detail/js/main.js
+++ detail/assets_detail/js/main.js
@@ -24,3 +24,53 @@ let swiperCards = new Swiper(".card__content", {
     },
   },
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+   const sections = document.querySelectorAll(".hidden");
+
+   const observer = new IntersectionObserver(
+      (entries, observer) => {
+         entries.forEach(entry => {
+            if (entry.isIntersecting) {
+               entry.target.classList.add("fade-in");
+            } else {
+               entry.target.classList.remove("fade-in"); // Agar animasi diputar lagi saat kembali
+            }
+         });
+      },
+      { threshold: 0.1 } // Triggers saat 10% elemen terlihat
+   );
+
+   sections.forEach(section => {
+      observer.observe(section);
+   });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const previewTriggers = document.querySelectorAll('.preview-trigger');
+    const modal = document.querySelector('.preview-modal');
+    const modalImage = document.querySelector('.preview-img-lg');
+    const closeBtn = document.querySelector('.close');
+
+    // Event listener untuk setiap gambar yang diklik
+    previewTriggers.forEach(trigger => {
+        trigger.addEventListener('click', function () {
+            const imgSrc = this.src; // Mendapatkan src gambar yang diklik
+            modal.classList.add('active'); // Tambahkan kelas active untuk menampilkan modal
+            modalImage.src = imgSrc; // Tampilkan gambar di modal
+        });
+    });
+
+    // Menutup modal saat tombol close diklik
+    closeBtn.addEventListener('click', function () {
+        modal.classList.remove('active'); // Hilangkan kelas active untuk menyembunyikan modal
+    });
+
+    // Menutup modal jika pengguna mengklik di luar gambar
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.classList.remove('active'); // Hilangkan kelas active untuk menyembunyikan modal
+        }
+    });
+});
